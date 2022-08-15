8b45c0d8d90188ee5933dca8f990eb9ae85b26ee
Kinga Śniegoń <kinga.sniegon@dieboldnixdorf.com>
2022-08-15 13:57:39 +0200

    Missed "BorrowedBook" class

---
+++ Library/BorrowedBook.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Library
+{
+    public class BorrowedBook
+    {
+
+        public string Username { get; set; }
+        public int BorrowedBookId { get; set; }
+        
+
+        public BorrowedBook (string username, int borrowedBookId)
+        {
+           Username = username;
+           BorrowedBookId = borrowedBookId;
+        }
+        
+      
+            
+    }
+}
